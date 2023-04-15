476b267e6cc6014e81b887d10e688fc71c1560b6
fernan344 <fernandoguerra344@gmail.com>
2023-04-15 09:47:02 -0600

    fix uri to server

--- services/server.service.js
+++ services/server.service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create(
     {
-        baseURL: 'http://localhost:3000/api',
+        baseURL: '',
         timeout: 15000,
         headers: {
             'Content-Type': 'application/json'
